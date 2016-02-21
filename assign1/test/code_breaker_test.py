ec553676808da3729c8266344fc11037a0347dcb
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-02-21 09:36:06 +0000

    do basic testing, and request reviews, Sikender you should take a look
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@644 0075fe68-b475-0747-8045-f8a20e60c073

--- assign1/test/code_breaker_test.py
+++ assign1/test/code_breaker_test.py
@@ -1,5 +1,34 @@
 import unittest
+import src.code_breaker
 
 class CodeBreakerTest(unittest.TestCase):
-  def test_canary(self):
-    self.assertTrue(True)
+	def test_canary(self):
+		self.assertTrue(True)
+
+	def setUp(self):
+		self.codeBreaker = src.code_breaker.CodeBreaker()
+
+	def tearDown(self):
+		pass
+
+	def test_new_game_has_20_chances(self):
+		self.assertEqual(20, self.codeBreaker.chances_left)
+
+	def result_of_guess(self, game, selection, guess):
+		game.setSelection(selection)
+		return game.guess(guess)
+
+	def test_game_return_5_blacks_for_correct_guess(self):
+		selection_set = ['Pink', 'Red', 'Green', 'Blue', 'Orange']
+		self.assertEqual(['Black', 'Black', 'Black', 'Black', 'Black'], \
+			self.result_of_guess(self.codeBreaker, selection_set, \
+				selection_set))
+
+	def test_game_return_5_whites_for_all_wrong_guess(self):
+		selection_set = ['Pink', 'Red', 'Green', 'Blue', 'Orange']
+		selection_guess = ['Black', 'White', 'Purple', 'Yellow', 'Silver']
+		self.assertEqual(['White', 'White', 'White', 'White', 'White'], \
+			self.result_of_guess(self.codeBreaker, selection_set,  selection_guess))
+
+		
+
