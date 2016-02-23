f14fa3c4032cbdb13e7465256399886344590cc5
sikendershahid91@gmail.com <sikendershahid91@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-02-23 01:01:49 +0000

    Made edits
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@711 0075fe68-b475-0747-8045-f8a20e60c073

--- assign1/src/code_breaker.py
+++ assign1/src/code_breaker.py
@@ -1,48 +1,49 @@
 from enum import Enum
 
-class Color(Enum):   ### for now i think should keep the essential colors black, silver, white
-	black = 0    ### perform tests to see if the response for two main cases works 
-	white = 1    ###  case 1: response for all win, response for all wrong
-	silver = 2
-	red = 3
-	green = 4    ### i was thinking about the color and the rgb value, ... notice in venkat's lecture
-	blue = 5     ### for the tic tac toe, he cared about the "X" and "O" case at the very end. 
-	pink = 6     ### only essential thing about the color pool should be right is distinction. 
-	yellow = 7   ###  test to see if color pool contains 10 distinctive colors. 
-	orange = 8   ###  represent the colors with decimal digits at the moment
-	purple = 9
-			
+
+class Color(Enum):
+    black = 0
+    white = 1
+    silver = 2
+    red = 3
+    green = 4
+    blue = 5
+    pink = 6
+    yellow = 7
+    orange = 8
+    purple = 9
+
+
 class Response(Enum):
-	MATCH = 0
-	MATCH_POSITION = 1
-	NO_MATCH = 2
+    MATCH = 0
+    MATCH_POSITION = 1
+    NO_MATCH = 2
+
 
 class CodeBreaker():
+    def __init__(self):
+        self.selected_colors = []
+        self.chances_left = 20
 
+    @staticmethod
+    def _check_color_selection_type(input_selection):
+        if len(input_selection) != 5:
+            raise (ValueError('Not exactly 5 colors.'))
+        return
 
-	def __init__(self):
-		self.chances_left = 20
+    def set_selected_colors(self, input_selection):
+        self._check_color_selection_type(input_selection)
+        self.selected_colors = input_selection
 
-	def _check_color_selection_type(self, input_selection):
-		if len(input_selection) != 5:
-			raise(ValueError('Not exactly 5 colors.'))
-		for i in range(5):
-			if type(input_selection[i]) != Color:
-				raise(ValueError('Input selection member is not Color type.'))
-		return
+    def evaluate_guess(self, input_guess):
+        self._check_color_selection_type(input_guess)
+        if self.selected_colors != input_guess:
+            key_sort_func = lambda x: x.value
+            if sorted(self.selected_colors, key=key_sort_func) == sorted(input_guess, key=key_sort_func):
+                return [Response.MATCH_POSITION] * 5
+            return [Response.NO_MATCH] * 5
+        return [Response.MATCH] * 5
 
-	def setSelection(self, input_selection):
-		self._check_color_selection_type(input_selection) 
-		self.selection = input_selection
-			
-	def guess(self, input_guess):
-		self._check_color_selection_type(input_guess)
-		if self.selection != input_guess:
-			key_sort_func = lambda x: x.value
-			if sorted(self.selection, key = key_sort_func) \
-				== sorted(input_guess, key = key_sort_func):
-				return [Response.MATCH_POSITION] * 5
-			return [Response.NO_MATCH] * 5
-		return [Response.MATCH] * 5
-
-
+    def select_color(self, param):
+        self.selected_colors.append(param)
+        return self.selected_colors[0]
--- assign1/test/code_breaker_test.py
+++ assign1/test/code_breaker_test.py
@@ -1,103 +1,49 @@
 import unittest
 from src.code_breaker import CodeBreaker, Color, Response
 
+
 class CodeBreakerTest(unittest.TestCase):
-	def test_canary(self):
-		self.assertTrue(True)
+    def test_canary(self):
+        self.assertTrue(True)
 
-	def setUp(self):
-		self.codeBreaker = CodeBreaker()
+    def setUp(self):
+        self.codeBreaker = CodeBreaker()
 
-	def result_of_guess(self, game, selection, guess):
-		game.setSelection(selection)
-		return game.guess(guess)
+    def select_first_color(self):
+        self.codeBreaker.select_color(Color.black)
+        self.assertEqual(Color.black, self.codeBreaker.selected_colors[0])
 
-	def test_set_selection_with_less_than_5_colors(self):
-		selection_set = [Color.black] * 4
-		try:
-			self.codeBreaker.setSelection(selection_set)
-			raise RuntimeError("setSelection accepts less than 5 colors.")
-		except ValueError:
-			pass
+    def test_guess_less_than_5_selection(self):
+        self.codeBreaker.select_color(Color.black)
+        self.codeBreaker.select_color(Color.white)
+        self.codeBreaker.select_color(Color.silver)
+        try:
+            self.codeBreaker.evaluate_guess(self.codeBreaker.selected_colors)
+            raise RuntimeError("guess accepts less than 5 colors.")
+        except ValueError:
+            pass
 
-	def test_set_selection_with_more_than_5_colors(self):
-		selection_set = [Color.black] * 6
-		try:
-			self.codeBreaker.setSelection(selection_set)
-			raise RuntimeError("setSelection accepts more than 5 colors.")
-		except ValueError:
-			pass
+    def test_guess_with_more_than_5_colors(self):
+        selection_set = [Color.black] * 6
+        try:
+            self.codeBreaker.evaluate_guess(selection_set)
+            raise RuntimeError("guess accepts more than 5 colors.")
+        except ValueError:
+            pass
 
-	def test_set_selection_with_variable_wrong_type(self):
-		selection_set = [Color.black] * 4
-		selection_set.append(0)
-		try:
-			self.codeBreaker.setSelection(selection_set)
-			raise RuntimeError("setSelection accepts one variable wrong type.")
-		except ValueError:
-			pass
+    def test_all_match_for_correct_guess(self):
+        selection_set = [Color.pink, Color.red, Color.green, Color.blue, Color.orange]
+        self.codeBreaker.set_selected_colors(selection_set)
+        self.assertEqual([Response.MATCH] * 5, self.codeBreaker.evaluate_guess(selection_set))
 
+    def test_game_all_no_match_for_all_different_colors(self):
+        selection_set = [Color.pink, Color.red, Color.green, Color.blue, Color.orange]
+        self.codeBreaker.set_selected_colors(selection_set)
+        selection_guess = [Color.black, Color.white, Color.purple, Color.yellow, Color.silver]
+        self.assertEqual([Response.NO_MATCH] * 5, self.codeBreaker.evaluate_guess(selection_guess))
 
-	def test_guess_with_less_than_5_colors(self):
-		selection_set = [Color.black] * 5
-		selection_guess = [Color.white] * 4
-		try:
-			return_response = self.result_of_guess(self.codeBreaker, \
-				selection_set, selection_guess)
-			raise RuntimeError("guess accepts less than 5 colors.")
-		except ValueError:
-			pass
-
-	def test_guess_with_more_than_5_colors(self):
-		selection_set = [Color.black] * 5
-		selection_guess = [Color.white] * 6
-		try:
-			return_response = self.result_of_guess(self.codeBreaker, \
-				selection_set, selection_guess)
-			raise RuntimeError("guess accepts more than 5 colors.")
-		except ValueError:
-			pass
-	### irrelevent 
-	def test_guess_with_variable_wrong_type(self):
-		selection_set = [Color.black] * 5
-		selection_guess = [Color.white] * 4
-		selection_guess.append('wrong type')
-		try:
-			return_response = self.result_of_guess(self.codeBreaker, \
-				selection_set, selection_guess)
-			raise RuntimeError("guess accepts one variable wrong type.")
-		except ValueError:
-			pass
-	
-	def test_guess_while_selection_not_set(self):
-		selection_guess = [Color.white] * 5
-		try:
-			return_response = self.codeBreaker.guess(selection_guess)
-			raise RuntimeError("Accept guess while selection is not set.")
-		except AttributeError:
-			pass
-
-	def test_game_all_match_for_correct_guess(self):
-		selection_set = [Color.pink, Color.red, Color.green, Color.blue, \
-			Color.orange]
-		self.assertEqual([Response.MATCH] * 5, self.result_of_guess(\
-			self.codeBreaker, selection_set, \
-				selection_set))
-
-	def test_game_all_no_match_for_all_different_colors(self):
-		selection_set = [Color.pink, Color.red, Color.green, Color.blue, \
-			Color.orange]
-		selection_guess = [Color.black, Color.white, Color.purple, \
-			Color.yellow, Color.silver]
-		self.assertEqual([Response.NO_MATCH]*5, self.result_of_guess(\
-			self.codeBreaker, selection_set, selection_guess))
-
-	def test_game_all_match_by_position_for_right_color_wrong_positions(self):
-		selection_set = [Color.pink, Color.red, Color.green, Color.blue, \
-			Color.orange]
-		selection_guess = [Color.red, Color.green, Color.blue, Color.orange, \
-			Color.pink]
-		self.assertEqual([Response.MATCH_POSITION]*5, self.result_of_guess(\
-			self.codeBreaker, selection_set, selection_guess))
-		
-
+    def test_game_all_match_by_position_for_right_color_wrong_positions(self):
+        selection_set = [Color.pink, Color.red, Color.green, Color.blue, Color.orange]
+        self.codeBreaker.set_selected_colors(selection_set)
+        selection_guess = [Color.red, Color.green, Color.blue, Color.orange, Color.pink]
+        self.assertEqual([Response.MATCH_POSITION] * 5, self.codeBreaker.evaluate_guess(selection_guess))
