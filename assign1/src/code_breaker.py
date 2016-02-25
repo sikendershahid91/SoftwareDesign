9d161b70342f7a6caf544f852f9590bd7679ee76
sikendershahid91@gmail.com <sikendershahid91@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-02-25 02:35:27 +0000

    made changes requesting a review.
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@805 0075fe68-b475-0747-8045-f8a20e60c073

--- assign1/src/code_breaker.py
+++ assign1/src/code_breaker.py
@@ -15,58 +15,52 @@ class Color(Enum):
 
 
 class Response(Enum):
-    MATCH_POSITION = 2
+    MATCH_POSITION = 0
     MATCH = 1
-    NO_MATCH = 0
+    NO_MATCH = 2
 
 
-class CodeBreaker():
+class Status(Enum):
+    WIN = 0
+    LOSE = 1
+    CONTINUE = 2
+
+
+class CodeBreaker:
     def __init__(self):
         self.selected_colors = []
-        self.response = [] #Venkat: Remove from the class
-        self.number_of_chances_left = 20
+        self.number_of_guess_made = 0
+        self.game_status = Status.CONTINUE
 
-    @staticmethod #Venkat: static is not a good idea. Make these instance methods
-    def _check_type_color(input_color): #Venkat: Remove
-        if type(input_color) != Color:
-            raise (ValueError("Not true color type."))
-
-    @staticmethod
-    def _check_color_selection_type(input_selection):  #Venkat: Remove
+    def set_colors_selection(self, input_selection):
         if len(input_selection) != 5:
             raise (ValueError('Not exactly 5 colors.'))
-        return
-
-    def set_selected_colors(self, input_selection):
-        self._check_color_selection_type(input_selection)
         self.selected_colors = input_selection
 
-    def match(self, input_guess):
-        self._check_color_selection_type(input_guess)
-        self.response = []
+    def guess_colors(self, input_guess):
+        if len(input_guess) != 5:
+            raise (ValueError('Not exactly 5 colors.'))
+        response = []
         left_over_selection_color = []
         left_over_guess_color = []
         for i in range(5):
             if self.selected_colors[i] == input_guess[i]:
-                self.response.append(Response.MATCH_POSITION)
+                response.append(Response.MATCH_POSITION)
             else:
                 left_over_selection_color.append(self.selected_colors[i])
                 left_over_guess_color.append(input_guess[i])
         for color in left_over_guess_color:
             if color in left_over_selection_color:
-                self.response.append(Response.MATCH)
+                response.append(Response.MATCH)
                 left_over_selection_color.remove(color)
             else:
-                self.response.append(Response.NO_MATCH)
-        self.response.sort(key=lambda x: x.value)
-        return self.response
+                response.append(Response.NO_MATCH)
+        response.sort(key=lambda x: x.value)
 
-    def guess(self, input_guess): #Venkat: Remove
-        self.number_of_chances_left -= 1
-        if self.number_of_chances_left < 0:
-            raise ValueError
-        return self.match(input_guess)
+        self.number_of_guess_made += 1
+        if self.number_of_guess_made <= 20 and response == [Response.MATCH_POSITION]*5:
+            self.game_status = Status.WIN
+        elif self.number_of_guess_made >= 20:
+            self.game_status = Status.LOSE
 
-    def color_selection(self, param):  #Venkat: Remove
-        self._check_type_color(param)
-        self.selected_colors.append(param)
+        return response
