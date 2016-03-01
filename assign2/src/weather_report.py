00ca621ad21fcca1ba7e68a2ef26a30298c40a66
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-01 07:34:43 +0000

    add abstract class for zipcode information, test with mock object
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@1126 0075fe68-b475-0747-8045-f8a20e60c073

--- assign2/src/weather_report.py
+++ assign2/src/weather_report.py
@@ -1,13 +1,30 @@
+import abc
+
+
+class ZipcodeInformation:
+    __metaclass__  = abc.ABCMeta
+
+    @abc.abstractmethod
+    def get_zipcode_location(self, zipcode):
+        raise NotImplemented
 
 
 class WeatherReport:
     def __init__(self):
         self.zipcode_list = []
+        self.zipcode_service = None
 
     def add_zipcode(self, read_zipcode):
         for zipcode in read_zipcode:
             if len(zipcode) != 5 or not(zipcode.isdigit()):
                 raise ValueError("Invalid Zipcode Error: Invalid number of digits")
-            else:
-                self.zipcode_list.append(zipcode)
-        return self.zipcode_list
+            self.zipcode_list.append(zipcode)
+
+    def set_location_service(self, service):
+        self.zipcode_service = service
+
+    def get_locations(self):
+        zipcode_locations = []
+        for zipcode in self.zipcode_list:
+            zipcode_locations.append(self.zipcode_service.get_zipcode_location(zipcode))
+        return zipcode_locations
--- assign2/test/weather_report_test.py
+++ assign2/test/weather_report_test.py
@@ -1,5 +1,6 @@
 import unittest
-from src.weather_report import WeatherReport
+from unittest.mock import patch
+from src.weather_report import WeatherReport, ZipcodeInformation
 
 
 class WeatherReportTest(unittest.TestCase):
@@ -10,15 +11,24 @@ class WeatherReportTest(unittest.TestCase):
         self.weatherReport = WeatherReport()
 
     def test_read_zipcode_5_digit(self):
-        self.assertEqual(['12345'], self.weatherReport.add_zipcode(["12345"]))
+        self.weatherReport.add_zipcode(["12345"])
+        self.assertEqual(['12345'], self.weatherReport.zipcode_list)
 
     def test_read_zipcode_not_5_digit_exception(self):
         self.assertRaises(ValueError, self.weatherReport.add_zipcode, ["123456"])
 
     def test_read_more_than_one_zipcode(self):
-        self.assertEqual(['12345', '54321'], self.weatherReport.add_zipcode(["12345", "54321"]))
+        self.weatherReport.add_zipcode(["12345", "54321"])
+        self.assertEqual(['12345', '54321'], self.weatherReport.zipcode_list)
 
     def test_read_zipcode_alphanumeric_type_exception(self):
         self.assertRaises(ValueError, self.weatherReport.add_zipcode, ["aa&12"])
+
+    @patch.object(ZipcodeInformation, 'get_zipcode_location')
+    def test_zipcode_location_Houston_when_service_working(self, mock_zipcode_info):
+        self.weatherReport.add_zipcode(['77004'])
+        mock_zipcode_info.get_zipcode_location.return_value = 'Houston'
+        self.weatherReport.set_location_service(mock_zipcode_info)
+        self.assertEqual(['Houston'], self.weatherReport.get_locations())
 
 
