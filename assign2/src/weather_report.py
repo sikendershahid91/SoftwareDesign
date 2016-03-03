524a7f6d4e55ba4579752178240df200a049f782
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-03 07:30:27 +0000

    I simplified the dictionary structure, doing three 3 tests using mocks like Venkat's review instructed. Removed some functions not sure what they do right now.
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@1227 0075fe68-b475-0747-8045-f8a20e60c073

--- assign2/src/weather_report.py
+++ assign2/src/weather_report.py
@@ -2,7 +2,7 @@ import abc
 
 
 class ZipcodeInformation:
-    __metaclass__  = abc.ABCMeta
+    __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
     def get_zipcode_location(self, zipcode):
@@ -11,20 +11,19 @@ class ZipcodeInformation:
 
 class WeatherReport:
     def __init__(self):
-        self.zipcode_list = []
-        self.zipcode_service = None
+        self.location_service = None
+        self.zipcode_location_dictionary = {}
 
-    def add_zipcode(self, read_zipcode):
+    def read_zipcodes(self, read_zipcode):
         for zipcode in read_zipcode:
             if len(zipcode) != 5 or not(zipcode.isdigit()):
-                raise ValueError("Invalid Zipcode Error: Invalid number of digits")
-            self.zipcode_list.append(zipcode)
+                raise ValueError(
+                    "Invalid Zipcode Error: Invalid number of digits")
+            else:
+                self.zipcode_location_dictionary[zipcode] = (
+                    self.location_service.get_zipcode_location(zipcode))
+        return self.zipcode_location_dictionary
 
     def set_location_service(self, service):
-        self.zipcode_service = service
+        self.location_service = service
 
-    def get_locations(self):
-        zipcode_locations = []
-        for zipcode in self.zipcode_list:
-            zipcode_locations.append(self.zipcode_service.get_zipcode_location(zipcode))
-        return zipcode_locations
--- assign2/test/weather_report_test.py
+++ assign2/test/weather_report_test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from src.weather_report import WeatherReport, ZipcodeInformation
 
 
@@ -10,25 +10,39 @@ class WeatherReportTest(unittest.TestCase):
     def setUp(self):
         self.weatherReport = WeatherReport()
 
-    def test_read_zipcode_5_digit(self):
-        self.weatherReport.add_zipcode(["12345"])
-        self.assertEqual(['12345'], self.weatherReport.zipcode_list)
-
-    def test_read_zipcode_not_5_digit_exception(self):
-        self.assertRaises(ValueError, self.weatherReport.add_zipcode, ["123456"])
-
-    def test_read_more_than_one_zipcode(self):
-        self.weatherReport.add_zipcode(["12345", "54321"])
-        self.assertEqual(['12345', '54321'], self.weatherReport.zipcode_list)
-
-    def test_read_zipcode_alphanumeric_type_exception(self):
-        self.assertRaises(ValueError, self.weatherReport.add_zipcode, ["aa&12"])
+    # def test_read_zipcode_5_digit(self):
+    #     self.weatherReport.add_zipcode(["12345"])
+    #     self.assertEqual(['12345'], self.weatherReport.zipcode_list)
 
     @patch.object(ZipcodeInformation, 'get_zipcode_location')
-    def test_zipcode_location_Houston_when_service_working(self, mock_zipcode_info):
-        self.weatherReport.add_zipcode(['77004'])
-        mock_zipcode_info.get_zipcode_location.return_value = 'Houston'
+    def test_read_one_zipcode_location_when_service_working(
+        self, mock_zipcode_info):
+        zipcode_list_to_read = ['77004']
+        mock_zipcode_info.get_zipcode_location.return_value = ('Houston', 'TX')
         self.weatherReport.set_location_service(mock_zipcode_info)
-        self.assertEqual(['Houston'], self.weatherReport.get_locations())
+        self.assertEqual({'77004' : ('Houston', 'TX')}, 
+            self.weatherReport.read_zipcodes(zipcode_list_to_read))
+
+    @patch.object(ZipcodeInformation, 'get_zipcode_location')
+    def test_read_three_zipcode_location_when_service_working(
+        self, mock_zipcode_info):
+        zipcode_list_to_read = ['77004', '90210', '10001']
+        mock_zipcode_info.get_zipcode_location.side_effect = [
+            ('Houston', 'TX'),
+            ("Beverly Hills", 'CA'),
+            ('New York', 'NY')]
+        self.weatherReport.set_location_service(mock_zipcode_info)
+        self.assertEqual(
+            {'77004' : ('Houston', 'TX'),
+            '90210' : ("Beverly Hills", 'CA'),
+            '10001' : ('New York', 'NY')},
+            self.weatherReport.read_zipcodes(zipcode_list_to_read))
+
+    def test_read_0_zipcode_service_not_called(self):
+        zipcode_list_to_read = []
+        self.assertEqual({},
+            self.weatherReport.read_zipcodes(zipcode_list_to_read))
+
+
 
 
