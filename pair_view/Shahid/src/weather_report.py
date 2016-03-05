9b04564c8b6ee90234fded3aa1754ceed7ff619f
sikendershahid91@gmail.com <sikendershahid91@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-05 06:55:04 +0000

    pulling back previous version
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@1339 0075fe68-b475-0747-8045-f8a20e60c073

--- pair_view/Shahid/src/weather_report.py
+++ pair_view/Shahid/src/weather_report.py
@@ -20,13 +20,63 @@ class WeatherReport:
             if len(zipcode) != 5 or not(zipcode.isdigit()):
                 raise ValueError("Invalid Zipcode Error: Invalid number of digits")
             else:
-                self.zipcode_dictionary = {zipcode: {'city': 'empty_data', 'state': 'empty_data'}}
+                self.zipcode_dictionary = {zipcode: {
+                'city': 'empty_data', 
+                'state': 'empty_data',
+                'temperature_minimum': 'empty_data',
+                'temperature_maximum': 'empty_data',
+                'weather_condition': 'empty_data',
+                }}
         return self.zipcode_dictionary
-
+ 
     def read_list(self):
         pass
 
     def set_zipcode(self, zipcode, zipcode_data):
         if zipcode != " ":
             self.zipcode_dictionary[zipcode] = zipcode_data
+
+
+
+class WeatherReportServiceInterface:
+    def get_zipcode_weather(self, zipcode):
+        pass
+
+    def get_zipcode_location(self, zipcode):
+        pass
+
+
+class WeatherReport:
+    def __init__(self):
+        self.zipcode_service = None
+        self.zipcode_dictionary = {}
+
+    def read_zipcode(self, input_zipcode_list):
+        for zipcode in input_zipcode_list:
+            if len(zipcode) != 5 or not (zipcode.isdigit()):
+                raise ValueError(
+                    "Invalid Zipcode Error: Invalid number of digits")
+            else:
+                self.zipcode_dictionary = {zipcode: {
+                    'city': 'empty_data',
+                    'state': 'empty_data',
+                    'temperature_minimum': 'empty_data',
+                    'temperature_maximum': 'empty_data',
+                    'weather_condition': 'empty_data',
+                }}
+        return self.zipcode_dictionary
+
+    def set_zipcode_location(self, zipcode, zipcode_location):
+        self.zipcode_dictionary[zipcode]['city'] = zipcode_location['city']
+        self.zipcode_dictionary[zipcode]['state'] = zipcode_location['state']
+
+    def set_zipcode_weather(self, zipcode, zipcode_weather):
+        self.zipcode_dictionary[zipcode]['temperature_minimum'] = zipcode_weather['temperature_minimum']
+        self.zipcode_dictionary[zipcode]['temperature_maximum'] = zipcode_weather['temperature_maximum']
+        self.zipcode_dictionary[zipcode]['weather_condition'] = zipcode_weather['weather_condition']
+
+        # self.zipcode_dictionary[zipcode] = (
+        #                     self.zipcode_service.get_zipcode_location(zipcode))
+        #                 self.zipcode_dictionary[zipcode] = (
+        #                     self.zipcode_service.get_zipcode_weather(zipcode))
 
--- pair_view/Shahid/test/weather_report_test.py
+++ pair_view/Shahid/test/weather_report_test.py
@@ -45,3 +45,78 @@ class WeatherReportTest(unittest.TestCase):
         zipcode_list = self.weatherReport.read_list.return_value = [" "]
         self.weatherReport.add_zipcode(zipcode_list)
         self.assertEqual({}, self.weatherReport.zipcode_dictionary)
+
+
+        import unittest
+from unittest.mock import patch, Mock
+from src.weather_report import WeatherReport, WeatherReportServiceInterface
+
+
+class WeatherReportTest(unittest.TestCase):
+    def test_canary(self):
+        self.assertTrue(self)
+
+    def setUp(self):
+        self.weatherReport = WeatherReport()
+
+    def test_read_zipcode_5_digit(self):
+        self.weatherReport.read_zipcode(["12345"])
+        self.assertEqual({
+                    'city': 'empty_data',
+                    'state': 'empty_data',
+                    'temperature_minimum': 'empty_data',
+                    'temperature_maximum': 'empty_data',
+                    'weather_condition': 'empty_data',
+                }, self.weatherReport.zipcode_dictionary['12345'])
+
+    @patch.object(WeatherReportServiceInterface, 'get_zipcode_location')
+    def test_read_one_zipcode_location_when_service_working(self, mock_zipcode_info):
+        self.weatherReport.read_zipcode(['77004'])
+        mock_zipcode_info.get_zipcode_location.return_value = {'city': 'Houston', 'state': 'TX'}
+        self.weatherReport.set_zipcode_location('77004', mock_zipcode_info)
+        print(self.weatherReport.zipcode_dictionary['77004']['city'])
+        self.assertEqual('Houston', self.weatherReport.zipcode_dictionary['77004']['city'])
+
+    @patch.object(WeatherReportServiceInterface, 'get_zipcode_location')
+    def test_read_three_zipcode_location_when_service_working(self, mock_zipcode_info):
+        self.weatherReport.read_zipcode(['77004', '90210', '10001'])
+        mock_zipcode_info.get_zipcode_location.side_effect = [
+            {'city_data': 'Houston', 'state_data': 'TX'},
+            {'city_data': 'Beverly Hills', 'state_data': 'CA'},
+            {'city_data': 'New York', 'state_data': 'TX'}]
+        self.weatherReport.set_zipcode_location('77004', mock_zipcode_info)
+        self.weatherReport.set_zipcode_location('90210', mock_zipcode_info)
+        self.weatherReport.set_zipcode_location('10001', mock_zipcode_info)
+        self.assertEqual(
+            {{'city_data': 'Houston', 'state_data': 'TX'},
+             {'city_data': 'Beverly Hills', 'state_data': 'CA'},
+             {'city_data': 'New York', 'state_data': 'TX'}},
+            self.weatherReport.zipcode_dictionary['77004', '90210', '10001'])
+
+    def test_read_0_zipcode_service_not_called(self):
+        zipcode_list_to_read = []
+        self.assertEqual({}, self.weatherReport.read_zipcode(zipcode_list_to_read))
+
+    @patch.object(WeatherReportServiceInterface, 'get_zipcode_weather')
+    def test_read_one_zipcode_weather_when_service_working(self, mock_zipcode_info):
+        self.weatherReport.read_zipcode(['77004'])
+        mock_zipcode_info.get_zipcode_weather.return_value = {'city_data': 'Houston', 'state_data': 'TX'}
+        self.weatherReport.set_zipcode_weather(mock_zipcode_info)
+        self.assertEqual({'city_data': 'Houston', 'state_data': 'TX'}, self.weatherReport.zipcode_dictionary['77004'])
+
+    @patch.object(WeatherReportServiceInterface, 'get_zipcode_weather')
+    def test_read_three_zipcode_weather_when_service_working(self, mock_zipcode_info):
+        self.weatherReport.read_zipcode(['77004', '90210', '10001'])
+        mock_zipcode_info.get_zipcode_weather.side_effect = [
+            {'temperature_minimum': '20', 'temperature_maximum': '25', 'weather_condition': 'Mostly Sunny'},
+            {'temperature_minimum': '25', 'temperature_maximum': '30', 'weather_condition': 'Sunny'},
+            {'temperature_minimum': '30', 'temperature_maximum': '40', 'weather_condition': 'Partially Sunny'}]
+        self.weatherReport.set_zipcode_weather(mock_zipcode_info)
+        self.assertEqual(
+            {{'temperature_minimum': '20', 'temperature_maximum': '25', 'weather_condition': 'Mostly Sunny'},
+             {'temperature_minimum': '20', 'temperature_maximum': '25', 'weather_condition': 'Mostly Sunny'},
+             {'temperature_minimum': '20', 'temperature_maximum': '25', 'weather_condition': 'Mostly Sunny'}},
+            self.weatherReport.zipcode_dictionary['77004', '90210', '10001'])
+
+
+
