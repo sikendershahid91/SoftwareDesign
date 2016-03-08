7aab8b4539f62d30f5467521678f49b1a470dce0
svenkat@cs.uh.edu <svenkat@cs.uh.edu@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-08 12:07:25 +0000

    include coverage report in html
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@1494 0075fe68-b475-0747-8045-f8a20e60c073

--- assign2/pavement.py
+++ assign2/pavement.py
@@ -9,7 +9,7 @@ import shutil
 
 @task
 def test():
-    sh('nosetests --with-coverage test')
+    sh('nosetests --with-coverage --cover-html test')
     pass
 
 
@@ -18,6 +18,7 @@ def clean():
     for pycfile in glob.glob("*/*/*.pyc"): os.remove(pycfile)
     for pycache in glob.glob("*/__pycache__"): os.removedirs(pycache)
     for pycache in glob.glob("./__pycache__"): shutil.rmtree(pycache)
+    for cover in glob.glob("./cover"): shutil.rmtree(cover)
     pass
 
 
