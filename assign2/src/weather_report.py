1e85b262de4c45aa9df2b1758f45d79bbe04c38e
svenkat@cs.uh.edu <svenkat@cs.uh.edu@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-08 13:00:09 +0000

    assign2 synopsis: nice progress
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@1499 0075fe68-b475-0747-8045-f8a20e60c073

--- assign2/src/weather_report.py
+++ assign2/src/weather_report.py
@@ -1,18 +1,16 @@
-
-
 class WeatherReport:
     def __init__(self):
         self.zipcode_service = None
 
-    def read_zipcodes(self, read_zipcode): 
+    def read_zipcodes(self, read_zipcode): #Venkat: change to get_cities_and_state. Change parameter to zipcodes
         return dict(map(
             lambda x: (x, self.zipcode_service.get_zipcode_location(x)),
             read_zipcode))
 
-    def read_weather(self, read_zipcode):
+    def read_weather(self, read_zipcode): #Venkat: change to get_weather_data. Change parameter to zipcodes
         return dict(map(
             lambda x: (x, self.zipcode_service.get_zipcode_weather(x)),
             read_zipcode))
-
+#Venkat: The weather data should come from a different service than the citie and states details. SRP.
     def set_service(self, service):
         self.zipcode_service = service
