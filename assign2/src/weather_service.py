297422dbb27579ff65eb64b864263973f8024a14
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-09 12:29:59 +0000

    weather service test
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@1544 0075fe68-b475-0747-8045-f8a20e60c073

--- assign2/src/weather_service.py
+++ assign2/src/weather_service.py
@@ -1,3 +1,33 @@
+import requests
+import xml.etree.ElementTree as ET
+
 class WeatherService:
+    def __init__(self):
+        self.weather_gov_url = (
+            'http://graphical.weather.gov/xml/sample_products/browser_interface'
+            '/ndfdBrowserClientByDay.php?'
+            'format=24+hourly&numDays=1&zipCodeList=')
+
     def get_zipcode_weather(self, zipcode):
-        raise NotImplemented
+        if len(zipcode) != 5 or not(zipcode.isdigit()):
+            return 'INVALID'
+        response = requests.get(self.weather_gov_url + zipcode)
+        if response.status_code != 200:
+            return 'ERROR'
+
+        tree_data = ET.fromstring(response.content)
+
+        for condition in tree_data.iter('weather-conditions'):
+            weather_condition = condition.attrib['weather-summary']
+
+        maximum_temp = 0
+        minimum_temp = 0
+        for temperature in tree_data.iter('temperature'):
+            if temperature.attrib['type'] == 'maximum':
+                maximum_temp = temperature.find('value').text
+            if temperature.attrib['type'] == 'minimum':
+                minimum_temp = temperature.find('value').text
+
+        return (minimum_temp, maximum_temp, weather_condition)
+
+
--- assign2/test/weather_service_test.py
+++ assign2/test/weather_service_test.py
@@ -3,8 +3,31 @@ from unittest.mock import patch, Mock
 from src.weather_service import WeatherService
 
 
-class LocationServiceTest(unittest.TestCase):
+class WeatherServiceTest(unittest.TestCase):
 
     def setUp(self):
-        self.weatherReport = WeatherReport()
         self.weatherService = WeatherService()
+
+    def test_get_invalid_from_service_for_wrong_zipcode_digit(self):
+        self.assertEqual('INVALID', 
+            self.weatherService.get_zipcode_weather('a2342'))
+
+    def test_get_invalid_from_service_for_wrong_zipcode_length(self):
+        self.assertEqual('INVALID', 
+            self.weatherService.get_zipcode_weather('122342'))
+
+    @patch('src.weather_service.requests.get')
+    def test_not_succesful_http_connection_output(self, mock_get):
+        mock_response = Mock(status_code = 404)
+        mock_get.return_value = mock_response
+        self.assertEqual('ERROR', 
+            self.weatherService.get_zipcode_weather('77004'))
+
+    @patch('src.location_service.requests.get')
+    def test_get_correct_location_from_successful_json_data(self, mock_get):
+        mock_response = Mock(status_code = 200)
+        with open('test/weather_service_successful_response.xml', 'r') as f:
+            mock_response.content = f.read()
+        mock_get.return_value = mock_response
+        self.assertEqual(('41', '48', 'Rain'), 
+            self.weatherService.get_zipcode_weather('77004'))
