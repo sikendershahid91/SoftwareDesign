f0b53f755d5e1c17a2aabfa0417130253a01154b
svenkat@cs.uh.edu <svenkat@cs.uh.edu@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-09 14:45:03 +0000

    assign2 synopsis: very nice
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@1557 0075fe68-b475-0747-8045-f8a20e60c073

--- assign2/src/location_service.py
+++ assign2/src/location_service.py
@@ -9,16 +9,23 @@ class LocationService:
     def get_zipcode_location(self, zipcode):
         if len(zipcode) != 5 or not(zipcode.isdigit()):
             return 'INVALID'
+            
         response = requests.get(self._google_api_url + zipcode)
+        
         if response.status_code != 200:
             return 'ERROR'
+            
         json_data = response.json()
+        
         address_components = json_data['results'][0]['address_components']
+        
         for key, value in enumerate(address_components):
             if 'locality' in value['types']:
                 city = value['short_name']
             if 'administrative_area_level_1' in value['types']:
                 state = value['short_name']
 
-        state = 'TX'
+        state = 'TX' #Venkat: Yes, Texas is special, but let's not hard code that! :)
         return (city, state)
+#Venkat: Notice the line breaks I added to improve readability. Different steps need line breaks.
+#Just noticed that you have done that quite well in WeatherService. That's what we want.
