95bd632b7f2080a5b3c3b438615ec7548dbc89e0
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-09 10:31:56 +0000

    remove weather report comments
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@1538 0075fe68-b475-0747-8045-f8a20e60c073

--- assign2/src/weather_report.py
+++ assign2/src/weather_report.py
@@ -3,16 +3,16 @@ class WeatherReport:
         self.location_service = None
         self.weather_service = None
 
-    def get_cities_and_state(self, zipcodes): #Venkat: change to get_cities_and_state. Change parameter to zipcodes
+    def get_cities_and_state(self, zipcodes):
         return dict(map(
             lambda x: (x, self.location_service.get_zipcode_location(x)),
             zipcodes))
 
-    def get_weather_data(self, zipcodes): #Venkat: change to get_weather_data. Change parameter to zipcodes
+    def get_weather_data(self, zipcodes):
         return dict(map(
             lambda x: (x, self.weather_service.get_zipcode_weather(x)),
             zipcodes))
-#Venkat: The weather data should come from a different service than the citie and states details. SRP.
+
     def set_location_service(self, service):
         self.location_service = service
 
