b615405888d8a1e6f5303ac5da5f49e658c0196c
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-26 08:27:26 +0000

    remove unncesscary code, and request reviews
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@2084 0075fe68-b475-0747-8045-f8a20e60c073

--- assign3/src/fibonacci_recursive.py
+++
@@ -1,6 +0,0 @@
-def fibonacci_recursive(number):
-    if number < 0:
-        raise ValueError
-
-    return 1 if number in [0, 1] else (
-    	fibonacci_recursive(number - 2) + fibonacci_recursive(number - 1))
