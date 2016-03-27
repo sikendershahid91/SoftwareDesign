8a0a17146ab67b4f191f649eb8be2bcae8eb9a27
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-27 17:16:54 +0000

    change recursive test and iterative test behavior to avoid DRY violation. However, solution is not optimal yet. Needs to be redone tonight.
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@2152 0075fe68-b475-0747-8045-f8a20e60c073

--- assign3/test/fibonacci_iterative_test.py
+++ assign3/test/fibonacci_iterative_test.py
@@ -2,30 +2,34 @@ import unittest
 from src.fibonacci_iterative import fibonacci_iterative
 
 
-class FibonacciTest(unittest.TestCase):
+class FibonacciIterativeTest(unittest.TestCase):
+
     def test_canary(self):
         self.assertTrue(self)
 
+    def setUp(self):
+        self.fibonacci = fibonacci_iterative
+
     def test_negative_position_throws_exception(self):
-        self.assertRaises(ValueError, fibonacci_iterative, -1)
+        self.assertRaises(ValueError, self.fibonacci, -1)
 
     def test_position_0_return_1(self):
-        self.assertEqual(1, fibonacci_iterative(0))
+        self.assertEqual(1, self.fibonacci(0))
 
     def test_position_1_return_1(self):
-        self.assertEqual(1, fibonacci_iterative(1))
+        self.assertEqual(1, self.fibonacci(1))
 
     def test_position_2_return_2(self):
-        self.assertEqual(2, fibonacci_iterative(2))
+        self.assertEqual(2, self.fibonacci(2))
 
     def test_position_4_return_5(self):
-        self.assertEqual(5, fibonacci_iterative(4))
+        self.assertEqual(5, self.fibonacci(4))
 
     def test_position_3_return_3(self):
-        self.assertEqual(3, fibonacci_iterative(3))
+        self.assertEqual(3, self.fibonacci(3))
 
     def test_position_5_return_8(self):
-        self.assertEqual(8, fibonacci_iterative(5))
+        self.assertEqual(8, self.fibonacci(5))
 
     def test_position_6_return_13(self):
-        self.assertEqual(13, fibonacci_iterative(6))        
+        self.assertEqual(13, self.fibonacci(6))        
--- assign3/test/fibonacci_recursive_test.py
+++ assign3/test/fibonacci_recursive_test.py
@@ -1,31 +1,9 @@
 import unittest
 from src.fibonacci_recursive import fibonacci_recursive
+from test.fibonacci_iterative_test import FibonacciIterativeTest
 
 
-class FibonacciTest(unittest.TestCase):
-    def test_canary(self):
-        self.assertTrue(self)
+class FibonacciRecursiveTest(FibonacciIterativeTest):
 
-    def test_negative_position_throws_exception(self):
-        self.assertRaises(ValueError, fibonacci_recursive, -1)
-
-    def test_position_0_return_1(self):
-        self.assertEqual(1, fibonacci_recursive(0))
-
-    def test_position_1_return_1(self):
-        self.assertEqual(1, fibonacci_recursive(1))
-
-    def test_position_2_return_2(self):
-        self.assertEqual(2, fibonacci_recursive(2))
-
-    def test_position_4_return_5(self):
-        self.assertEqual(5, fibonacci_recursive(4))
-
-    def test_position_3_return_3(self):
-        self.assertEqual(3, fibonacci_recursive(3))
-
-    def test_position_5_return_8(self):
-        self.assertEqual(8, fibonacci_recursive(5))
-
-    def test_position_6_return_13(self):
-        self.assertEqual(13, fibonacci_recursive(6))        
+    def setUp(self):
+        self.fibonacci = fibonacci_recursive
