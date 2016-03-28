55c5e282e4f0333db8915d46c8a233d9c7fb6f30
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-28 00:13:56 +0000

    new recursive, better written
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@2163 0075fe68-b475-0747-8045-f8a20e60c073

--- assign3/src/fibonacci_recursive.py
+++ assign3/src/fibonacci_recursive.py
@@ -2,4 +2,6 @@ def fibonacci_recursive(number):
     if number < 0:
         raise ValueError("Negative number!")
 
-    return 1 if number < 2 else fibonacci_recursive(number - 1) + fibonacci_recursive(number - 2)
+    return 1 if number < 2 else sum(map(
+    	fibonacci_recursive, 
+    	[number - 1, number - 2]))
