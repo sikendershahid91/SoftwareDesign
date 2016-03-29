27c663b57e4f3ada8930c7a76e8f12bbce42b19c
svenkat@cs.uh.edu <svenkat@cs.uh.edu@0075fe68-b475-0747-8045-f8a20e60c073>
2016-03-29 13:05:33 +0000

    assign3 synopsis: let's keep it DRY
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@2239 0075fe68-b475-0747-8045-f8a20e60c073

--- assign3/src/fibonacci_iterative.py
+++ assign3/src/fibonacci_iterative.py
@@ -6,4 +6,5 @@ def fibonacci_iterative(number):
 
     return reduce(
         lambda previous, index: [previous[1], sum(previous)],
-        range(number + 1), [1, 0]) [1]
+          range(number + 1), [1, 0]) [1]  #Venkat: I have added indentation to show this is part of the expression from the previous line. If l and r on the same veritcal indentation, that means they are two different expressions, but they are not. So indented r further.
+        
