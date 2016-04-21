99b294b8d213e5b20a7ebec9ef0bc9b4f09b417f
sikendershahid91@gmail.com <sikendershahid91@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-04-21 01:59:43 +0000

    edit on block_create
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@3079 0075fe68-b475-0747-8045-f8a20e60c073

--- assign4/src/block_create.py
+++ assign4/src/block_create.py
@@ -1,7 +1,10 @@
 from importlib import import_module
 
 def block_create(string):
-    file_name, class_name, parameters = string.split()
+     if string.count(' ') == 2:
+         file_name, class_name, parameters = string.split()
+     elif string.count(' ') == 1:
+         file_name, class_name = string.split()
 
     imported_module = import_module(file_name)
     class_type = getattr(imported_module, class_name)
--- assign4/src/main_character_encoder.py
+++ assign4/src/main_character_encoder.py
@@ -6,8 +6,13 @@ import sys
 def main():
     with open(sys.path[0] + '/filename.txt', 'r') as file:
         input_string = file.read().splitlines()
-    blocks = map(block_create(input_string), input_string)
-    print(blocks)
+        print(input_string)
+    for x in input_string:
+        print(x)
+        blocks = block_create(x)
+        print(blocks)
+    #blocks = map(block_create("{:s}".format(input_string)), input_string)
+    #print(blocks)
 
 
 if __name__ == '__main__':
