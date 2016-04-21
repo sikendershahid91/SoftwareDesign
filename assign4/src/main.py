08de78b3175ccd494973407eca9c536d3598c3c1
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-04-21 03:50:33 +0000

    main.py revert back to 3092
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@3095 0075fe68-b475-0747-8045-f8a20e60c073

--- assign4/src/main.py
+++ assign4/src/main.py
@@ -9,24 +9,18 @@ def get_blocks(file_path):
 
     return list(map(block_create, input_string))
 
-def get_input(file_path):
-    with open(file_path) as file:
-        input_string = file.read()
-    return input_string
-
 
 def main():
     file_name = 'user_blocks.txt'
     blocks = get_blocks(sys.path[0] + '/' + file_name)
-
+    
     print('Block sequence loaded from {}'.format(file_name))
     process_unit = ProcessingUnit(blocks)
 
-    file_name = 'user_input.txt'
-    input_string = get_input(sys.path[0] + '/' + file_name)
-    print('Input sequence loaded from {}'.format(file_name))
-    print('input string: {}'.format(input_string))
-    print('processed string: {}'.format(process_unit.process(input_string)))
+    while True:
+        print("String for process :: ", end = '')
+        print(process_unit.process(input()))
+
 
 if __name__ == '__main__':
     main()
