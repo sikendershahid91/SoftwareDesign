2f0216f4ce0c19a08490271f91e5e154d9093ce6
uduchi.2nd@gmail.com <uduchi.2nd@gmail.com@0075fe68-b475-0747-8045-f8a20e60c073>
2016-04-21 02:09:26 +0000

    revert block_create
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@3081 0075fe68-b475-0747-8045-f8a20e60c073

--- assign4/src/block_create.py
+++ assign4/src/block_create.py
@@ -1,10 +1,7 @@
 from importlib import import_module
 
 def block_create(string):
-     if string.count(' ') == 2:
-         file_name, class_name, parameters = string.split()
-     elif string.count(' ') == 1:
-         file_name, class_name = string.split()
+    file_name, class_name, *parameters = string.split()
 
     imported_module = import_module(file_name)
     class_type = getattr(imported_module, class_name)
