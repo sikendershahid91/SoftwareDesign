cfa9a9c137bb60ab5cd0a1419c3a568330b47c05
svenkat@cs.uh.edu <svenkat@cs.uh.edu@0075fe68-b475-0747-8045-f8a20e60c073>
2016-04-22 04:11:20 +0000

    assign4 synopsis: nice work
    git-svn-id: https://agile.cs.uh.edu/svn/s16_softwaredesign/nguyen_shahid@3167 0075fe68-b475-0747-8045-f8a20e60c073

--- assign4/src/main.py
+++ assign4/src/main.py
@@ -4,13 +4,13 @@ import sys
 
 
 def main():
-    file_name = 'user_blocks.txt'
+    file_name = 'user_blocks.txt' #Venkat: Can we move the user_blocks.txt file from src to assign4. This should not belong in the src directory.
     blocks = block_file_handle(sys.path[0] + '/' + file_name)
 
     print('Block sequence loaded from {}'.format(file_name))
     process_unit = ProcessingUnit(blocks)
 
-    while True:
+    while True: #Venkat: use a couple of hard coded input here for demo purpose. That way it can be easily automated in the run to show program working
         print("String for process :: ", end = '')
         print(process_unit.process(input()))
 
